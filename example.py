c991ab651c869e8214227cb0bab941b1350956af
Tomi <dev.tomi33@gmail.com>
2016-07-05 06:12:27 +0100

    Return dict objects and add child method
    Make the http request wrappers return a dict (json.loads()) as opposed to a json string.
    Add the child method to allow for changing the endpoint path in a more semantic way

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 import pyfire
-import pyfire.utils as utils
 import config
 
 # Example use of Pyfire
@@ -7,8 +6,8 @@ import config
 # Set URL endpoint and secret key (respectively) to the Firebase realtime database
 Pyfire = pyfire.Pyfire(config.FIREBASE_DATABASE_URL, config.FIREBASE_DATABASE_SECRET)
 
-new_object = Pyfire.post({'Foo': 'Bar'}) # Post an object to the Firebase realtime database. The object will be assigned an unique key. EG: "-KLYFXcwUO-rKJMwrT0F" and the stored object is returned
-new_object_name = utils.get_property_from_json_string('name', new_object) # Get the random key generated for the above object (can be used to add further children by changing the relative path to this)
+new_object = Pyfire.post({'Foo': 'Bar'})  # Post an object to the Firebase realtime database. The object will be assigned an unique key. EG: "-KLYFXcwUO-rKJMwrT0F" and the stored object is returned
+new_object_name = new_object['name']  # Get the random key generated for the above object (can be used to add further children by changing the relative path to this)
 
 Pyfire.set_endpoint_path(new_object_name) # Set path to the key of the item just created
 
@@ -19,4 +18,14 @@ Pyfire.patch({'more': 'data'})  # Add a new object into current path. This does 
 print Pyfire.set_endpoint_path('').get()  # Set endpoint path back to the root and get the contents. (This line demonstrates how certain request can be chained)
 
 # Get entity (user) with a specific value (abcdefg) for a specified key (prop_name)
-print Pyfire.set_endpoint_path('user').order_by("prop_name").equal_to("abcdefg").get()
+print Pyfire.set_endpoint_path('user').order_by("access_code").equal_to("abcdefg").get()
+
+Pyfire.set_endpoint_path('')  # reset path for example to make more sense
+
+# Use child method with variable amount of arguments
+print Pyfire.child('user', '-KLn5QoiyCLp3UQTIhXL', 'email_address').get()
+
+Pyfire.set_endpoint_path('')  # reset path for example to make more sense
+
+# Child with just one arg
+print Pyfire.child('user').get()
--- pyfire/utils.py
+++
@@ -1,5 +0,0 @@
-import json
-
-
-def get_property_from_json_string(prop_name, json_string):
-    return json.loads(json_string)[prop_name]
