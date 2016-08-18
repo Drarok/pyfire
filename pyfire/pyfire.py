bfe8c3133f09b86fb30ae38845ec4b0af76470de
Mat Gadd <source@catchall.drarok.com>
2016-08-18 11:11:52 +0100

    Improve consistency, tidy up _execute method, refactor query string code out to a separate method, add URL escaping

--- pyfire/pyfire.py
+++ pyfire/pyfire.py
@@ -16,47 +16,57 @@ class Pyfire:
         return Query(self, endpoint_path)
 
     def get(self, path, query_params = None):
-        return self._execute("GET", path, None, query_params)
+        return self._execute('GET', path, None, query_params)
 
     def post(self, path, data, query_params = None):
-        res = self._execute("POST", path, data, query_params)
+        res = self._execute('POST', path, data, query_params)
 
         if not isinstance(res, dict):
-            raise Exception(':(')
+            raise Exception('Unexpected return value %s - expected dict' % type(res))
 
         return res.get('name')
 
     def put(self, path, data, query_params = None):
-        return self._execute("PUT", path, data, query_params)
+        return self._execute('PUT', path, data, query_params)
 
     def patch(self, path, data, query_params = None):
-        return self._execute("PATCH", path, data, query_params)
+        return self._execute('PATCH', path, data, query_params)
 
     def delete(self, path, query_params = None):
-        return self._execute("DELETE", path, None, query_params)
+        return self._execute('DELETE', path, None, query_params)
 
     def _execute(self, request_method, path, data, query_params = None):
-        request_url = '%s/%s.json?auth=%s' % (self.root_data_url, path, self.database_secret)
-
-        if isinstance(query_params, dict) and len(query_params):
-            query_string = "&"
-            if query_params.get('orderBy'):
-                query_string += 'orderBy="%s"' % query_params.get('orderBy')
-                # Can only use equalTo with orderBy, so nest if statement
-                if query_params.has_key('equalTo'):
-                    query_string += '&equalTo="%s"' % query_params.get('equalTo')
-            request_url += query_string
-
-        headers = {}
-        if data is not None:
-            data = json.dumps(data)
-            headers.update({'Content-Type': 'application/json'})
+        request_url = '%s/%s.json?auth=%s%s' % (
+            self.root_data_url, path,
+            self.database_secret, self._query_string(query_params)
+        )
 
         # Specify the HTTP method using this header to make the code simpler,
         # and because urllib2 only seems to support GET and POST anyway.
-        headers.update({'X-HTTP-Method-Override': request_method.upper()})
+        headers = {
+            'X-HTTP-Method-Override': request_method.upper()
+        }
+
+        if data is not None:
+            data = json.dumps(data)
+            headers.update({'Content-Type': 'application/json'})
 
         req = urllib2.Request(request_url, data, headers)
         response = urllib2.urlopen(req).read()
 
         return json.loads(response)
+
+    def _query_string(self, query_params):
+        query_string = ''
+
+        if isinstance(query_params, dict) and len(query_params):
+            query_string = '&'
+            if query_params.has_key('orderBy'):
+                query_string += 'orderBy="%s"' % urllib2.quote(query_params.get('orderBy'))
+
+                # This is nested as you can only use equalTo with orderBy
+                if query_params.has_key('equalTo'):
+                    query_string += '&equalTo="%s"' % urllib2.quote(query_params.get('equalTo'))
+
+        return query_string
+
