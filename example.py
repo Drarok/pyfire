07f93a0779868ea1a5fda19c6e1c66ab37755d4e
Mat Gadd <source@catchall.drarok.com>
2016-08-18 11:19:11 +0100

    `Pyfire.Pyfire` looks a bit weird

--- example.py
+++ example.py
@@ -1,27 +1,27 @@
-import pyfire
+from pyfire import Pyfire
 import config
 
-# Set URL endpoint and secret key (respectively) to the Firebase realtime database
-Pyfire = pyfire.Pyfire(config.FIREBASE_DATABASE_URL, config.FIREBASE_DATABASE_SECRET)
+# Set URL endpoint and secret key to the Firebase realtime database
+fb = Pyfire(config.FIREBASE_DATABASE_URL, config.FIREBASE_DATABASE_SECRET)
 
 # Create an object in the Test collection
-id = Pyfire.post('Test', {'Foo': 'Bar'})
+id = fb.post('Test', {'Foo': 'Bar'})
 
 # Get the random key generated for the above object (can be used to add further
 # children), then replace everything under this key
-Pyfire.put('Test/' + id, {'bar': 'foo'})
+fb.put('Test/' + id, {'bar': 'foo'})
 
 # Add a new object. This does not overrite existing children, but just appends this one
-Pyfire.patch('Test/' + id, {'more': 'data'})
+fb.patch('Test/' + id, {'more': 'data'})
 
 # Get the contents.
-print Pyfire.get('Test/' + id)
+print fb.get('Test/' + id)
 
 # Get objects by querying - you will need to set up .indexOn for this to work
 try:
-    print Pyfire.query('Test').order_by('bar').equal_to('foo').get()
+    print fb.query('Test').order_by('bar').equal_to('foo').get()
 except Exception as e:
     print e
 
 # Delete the object we created
-Pyfire.delete('Test/' + id)
+fb.delete('Test/' + id)
